8b2583ab2c848320d0fdf41549d7b83c13e5dce2
Eduardo1105rg <eduardorojasgomez10@gmail.com>
2025-11-16 23:54:29 -0600

    correction

--- codigo/API/src/Routes/EstadisticasRoutes.js
+++ codigo/API/src/Routes/EstadisticasRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const { optenerPromedioDeCompraProveedores, optenerPromedioDeVentasPorCliente, optenerTopGananciaProductosPorAnio, optenerTopCantPedidoPorClientePorAnio, optenerTopCantPedidoPorProveedorPorAnio } = require('../Controllers/EstadisticasController');
 const router = express.Router();
+const {validarAutenticacion} = require('../Config/Utils/Autenticacion_utils');
 
 // Este es la ruta para obtener clientes.
 
 // Obtener todo los clientes mediante filtros.
-router.get('/promedioCompraProveedores/:nombreProveedor/:categoria/:sucursal', optenerPromedioDeCompraProveedores);
+router.get('/promedioCompraProveedores/:nombreProveedor/:categoria/:sucursal',validarAutenticacion, optenerPromedioDeCompraProveedores);
 
-router.get('/promedioVentasPorCliente/:nombreCliente/:categoria/:sucursal', optenerPromedioDeVentasPorCliente); // Obtener un cliente mediante su ID.
+router.get('/promedioVentasPorCliente/:nombreCliente/:categoria/:sucursal',validarAutenticacion, optenerPromedioDeVentasPorCliente); // Obtener un cliente mediante su ID.
 
-router.get('/topGananciaProductosPorAnio/:anioInicio/:anioFin/:sucursal', optenerTopGananciaProductosPorAnio);
-router.get('/topCantPedidoPorClientePorAnio/:anioInicio/:anioFin/:sucursal', optenerTopCantPedidoPorClientePorAnio);
+router.get('/topGananciaProductosPorAnio/:anioInicio/:anioFin/:sucursal',validarAutenticacion, optenerTopGananciaProductosPorAnio);
+router.get('/topCantPedidoPorClientePorAnio/:anioInicio/:anioFin/:sucursal',validarAutenticacion, optenerTopCantPedidoPorClientePorAnio);
 
-router.get('/topCantPedidoPorProveedorPorAnio/:anioInicio/:anioFin/:sucursal', optenerTopCantPedidoPorProveedorPorAnio);
+router.get('/topCantPedidoPorProveedorPorAnio/:anioInicio/:anioFin/:sucursal',validarAutenticacion, optenerTopCantPedidoPorProveedorPorAnio);
 
 module.exports = router;
